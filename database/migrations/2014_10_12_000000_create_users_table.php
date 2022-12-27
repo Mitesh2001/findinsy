ddece0f0e6b854e42bdea94f98b94f83a5a3de5e
Mitesh2001 <miteshladva.dgrs@gmail.com>
2022-12-27 15:10:14 +0530

    password column in users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->string('email')->nullable();
+            $table->string('password')->nullable();
             $table->bigInteger('mobile_number')->nullable()->default(0);
             $table->date('birth_date')->nullable();
             $table->bigInteger('otp')->nullable();
